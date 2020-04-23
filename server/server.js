6dff052323002f045e84ad1904be1366692aaede
alex.paclin <alex.paclin@gmail.com>
2020-04-23 13:27:18 +0500

    074 SERVER .env and middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
