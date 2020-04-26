2040764a5811d76fc2ac7a9142ddcc024f3b3967
alex.paclin <alex.paclin@gmail.com>
2020-04-26 22:13:08 +0500

    084 SERVER Getting products by ID

--- server/server.js
+++ server/server.js
@@ -41,6 +41,27 @@ app.post('/api/product/article', auth, admin, (req, res) => {
 	});
 });
 
+// /api/product/article?id=HSHSHSKSK,JSJSJSJS,SDSDHHSHDS,JSJJSDJ&type=single
+app.get('/api/product/articles_by_id', (req, res) => {
+	let type = req.query.type;
+	let items = req.query.id;
+
+	if (type === 'array') {
+		let ids = req.query.id.split(',');
+		items = [];
+		items = ids.map((item) => {
+			return mongoose.Types.ObjectId(item);
+		});
+	}
+
+	Product.find({ _id: { $in: items } })
+		.populate('brand')
+		.populate('wood')
+		.exec((err, docs) => {
+			return res.status(200).send(docs);
+		});
+});
+
 //=================================
 //              WOODS
 //=================================
